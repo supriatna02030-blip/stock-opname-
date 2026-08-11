7572fc67af9119677d9bfb0a5a57a5a2c92d7d8e
supriatna02030-blip <supriatna02030@gmail.com>
2026-08-11 19:17:51 +0700

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "so-pro-v9";
+const urlsToCache = [
+  "./",
+  "index2.html"
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
